10f31ea6940dfe6c527aa7e10077f0df6b588066
samiki-hub <samwel.mueller2003@gmx.de>
2023-06-25 17:12:54 +0200

    Buttons für CamControl
    Kamera kann jetzt auf Bodies zentriert werden (Lukas)

---
+++ Assets/Script/MoveOnButton.cs
@@ -0,0 +1,71 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Drawing;
+using UnityEngine;
+
+public class MoveOnButton : MonoBehaviour
+{
+    [SerializeField]
+    Camera cam;
+    static Vector3 posMid = Vector3.zero;
+    static Vector3 posRight = Vector3.zero;
+    static Vector3 posLeft = Vector3.zero;
+    static int current;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam.orthographicSize = 1.8f;
+        posLeft.x = -1;
+        posLeft.y = 2.5f;
+        posLeft.z = -1;
+        current = 0;
+        posMid.x = 0;
+        posMid.y = 2.6f;
+        posMid.z = -1.8f;
+        posRight.x = 1.2f;
+        posRight.y = 2.5f;
+        posRight.z = -1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    private void DoMove(int i)
+    {
+        switch (i)
+        {
+            case 0:
+                cam.orthographicSize = 1.8f;
+                transform.position = posMid;
+                break;
+            case 1:
+                cam.orthographicSize = 1.2f;
+                transform.position = posRight;
+                break;
+            case -1:
+                cam.orthographicSize = 1.2f;
+                transform.position = posLeft;
+                break;
+        }
+    }
+    public void Move(int i)
+    {
+        switch (i)
+        {
+            case 1:
+                if (current == 1)
+                    break;
+                current += 1;
+                DoMove(current);
+                break;
+            case -1:
+                if (current == -1)
+                    break;
+                current += -1;
+                DoMove(current);
+                break;
+        }
+    }
+}
